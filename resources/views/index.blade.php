bac30188c18b243ebcae7060266a13f7e71d4eac
wira_sajaa <wirasanjaya142@gmail.com>
2022-11-22 20:04:11 +0700

    FIRST COMMIT

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h3 class="fw-bold">Ini adalah content</h3>
+      </div>
+    </div>
+  </div>
+@endsection
