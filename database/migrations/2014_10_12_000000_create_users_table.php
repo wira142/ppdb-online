4199c647d9daa476f24cdfc088025bcc55eaa1ef
wira_sajaa <wirasanjaya142@gmail.com>
2022-11-23 14:19:23 +0700

    setup: build database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->default('(uuid())')->primary();
+            $table->uuid('id')->default('(uuid())')->unique()->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
+Auth::routes();
 
 Route::get('/', function () {
     return view('index');
 });
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
